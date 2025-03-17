df591129032275a999ba13024314ee16d7d2779d
Muhammad Atif khan <22pwbcs0906@uetpeshawar.edu.pk>
2025-03-18 03:31:27 +0500

    adding comments about promise and learn that mongose mostly models will return promise

--- src/server.js
+++ src/server.js
@@ -7,17 +7,26 @@ const app = express()
 
 // so i make an request of of post that when the user enter data from frontend then create an instance of user and check it with model then push data to database
 
-app.post('/signin' , (req , res)=>{
 
-    // here we need to add an instance of new user 
+// here i write async await why ? this is bcz every model of mongoose return a promise so we need to hanlde it suppose if i donot write async await here and i write age in alphabtes instead of numeric so this is working but if there is any error it always execute try does not go to catch but when we use async await and i enter wrong data in user instance against schema then it will throw an error
+
+app.post('/signin' ,async (req , res)=>{
+
+    // here we need to add an instance of new user
+   // User is a Mongoose model (likely defined using mongoose.model('User', userSchema)).
+  // To create a new document (record) in MongoDB, you must instantiate an object of the model.
+// The new keyword tells JavaScript to create a new object based on the User schema. 
     const user = new User({
-        firstName:"Muhammad Atif ",
+        firstName:"Muhammad Atif khan ",
         email:"abc@gmail.com",
-        age:"10"
+        age:"a"
     })
     // this will also return a promise 
  try{
-    user.save()
+
+    // this save function return a promise so thats why its must to handle with async await
+    // the await will wait for it that either it will accept the request or reject
+  await  user.save()
     res.send('Data is successfully added to db')
  }
  catch(err){
