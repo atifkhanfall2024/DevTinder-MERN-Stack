7742a0697c41f19d1c25a56ae56609bf9bf51dba
Muhammad Atif khan <22pwbcs0906@uetpeshawar.edu.pk>
2025-03-18 16:00:19 +0500

    Dynamic routing

---
+++ Homework.md
@@ -0,0 +1,5 @@
+# Dynamic Routing     18/3/2025   Tuesday
+
+  - Difference bw json and js object
+  - Add express.json() to handle end users request 
+  - Dynamic routing from frontend or from end users
--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ const Dbconnect =  require('./config/database')
 const User = require('./models/Userdata')
 const app = express()
 
+// expree.json is used to hanlde or read  json data comes from frontend 
+app.use(express.json())
+
 // we we run it so we see in console that it first listening the requests then make the connection but if the connection is not success then it listening the api but cannot connect with database we need to handle it when the connection is success then listening from the server other wise not 
 
 // so i make an request of of post that when the user enter data from frontend then create an instance of user and check it with model then push data to database
@@ -10,7 +13,8 @@ const app = express()
 
 // here i write async await why ? this is bcz every model of mongoose return a promise so we need to hanlde it suppose if i donot write async await here and i write age in alphabtes instead of numeric so this is working but if there is any error it always execute try does not go to catch but when we use async await and i enter wrong data in user instance against schema then it will throw an error
 
-app.post('/signin' ,async (req , res)=>{
+/*app.post('/signin' ,async (req , res)=>{
+
 
     // here we need to add an instance of new user
    // User is a Mongoose model (likely defined using mongoose.model('User', userSchema)).
@@ -19,7 +23,7 @@ app.post('/signin' ,async (req , res)=>{
     const user = new User({
         firstName:"Muhammad Atif khan ",
         email:"abc@gmail.com",
-        age:"a"
+        age:"100"
     })
     // this will also return a promise 
  try{
@@ -32,8 +36,34 @@ app.post('/signin' ,async (req , res)=>{
  catch(err){
     res.status(400).send("not connect successfully")
  }
+}) */
+
+
+ // suppose if the request come from user side mean from front end or from post man then what happen ?
+ // i sending the data from post man to hitting signup api then its check schema validation and if data in json format is correct then it will goes to DB 
+
+app.post('/signup' , async (req , res)=>{
+
+// we handle the frontend request from req.body but data is in json format this will give un defined 
+// undefined: this is because our server cannot read the json data
+//console.log(req.body)
+
+   const user = new User(req.body)
+   
+   try{
+
+    await user.save()
+    res.send('Data is Added to Database')
+   }
+   catch(err){
+    console.log(err.message);
+    res.status(400).send('Something went wrong'+ err.message)
+   }
+  
 })
-Dbconnect().then(()=>{
+
+
+   Dbconnect().then(()=>{
   //  throw console.error('error');
     
     console.log('Connect with database');
